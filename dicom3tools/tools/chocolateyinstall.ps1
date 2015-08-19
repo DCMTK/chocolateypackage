2092d64e3042a637160eb5b9eff226157e897c91
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2015-08-19 11:49:52 +0200

    First version of dicom3tools package

---
+++ dicom3tools/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'dicom3tools' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://www.dclunie.com/dicom3tools/workinprogress/winexe/dicom3tools_winexe_1.00.snapshot.20150717110018.zip' # download url
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
---
+++ dicom3tools/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'dicom3tools'
+$zipFileName = 'dicom3tools_winexe_1.00.snapshot.20150717110018.zip'
+
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName
