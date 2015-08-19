ff4f62b49250ba67ca246476eaff2b965af89aa6
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2015-08-19 10:57:19 +0200

    Added first version of dcmtk package

---
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'dcmtk' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk360/bin/dcmtk-3.6.0-win32-i386.zip' # download url
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
---
+++ dcmtk/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'dcmtk'
+$zipFileName = 'dcmtk-3.6.0-win32-i386.zip'
+
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName
