19239e95e960c3bd23926d9f95ba5878d66b85a8
Rickard Holmberg <richo@raysearchlabs.com>
2017-08-25 15:16:04 +0200

    Update to 3.6.2

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -2,6 +2,6 @@
 
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk360/bin/dcmtk-3.6.0-win32-i386.zip' # download url
+$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk362/bin/dcmtk-3.6.2-win64-dynamic.zip' # download url
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
--- dcmtk/tools/chocolateyuninstall.ps1
+++ dcmtk/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$packageName = 'dcmtk'
-$zipFileName = 'dcmtk-3.6.0-win32-i386.zip'
+$zipFileName = 'dcmtk-3.6.2-win64-dynamic.zip'
 
 Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName
