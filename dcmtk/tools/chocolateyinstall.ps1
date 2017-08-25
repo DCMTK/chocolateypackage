d81ded5c74e0a6cd128da3db1edbd321fa7df4a6
Rickard Holmberg <richo@raysearchlabs.com>
2017-08-25 15:28:16 +0200

    Add checksum and separate 32- and 64-bit versions

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@
 
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk362/bin/dcmtk-3.6.2-win64-dynamic.zip' # download url
+$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk362/bin/dcmtk-3.6.2-win32-dynamic.zip' # download url
+$url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/dcmtk362/bin/dcmtk-3.6.2-win64-dynamic.zip' # download url
 
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+Install-ChocolateyZipPackage $packageName $url -Checksum B7AABBEC6DC14F72BAE6A2CC688CB0C0 $toolsDir -Url64 $url64 -Checksum64 87B85476006510DE8308F0DB3475DBA2
