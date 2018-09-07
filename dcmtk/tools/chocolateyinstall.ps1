5e7d67b8758619a9c3646cc91ccd438fa05c2ac4
Pedro ArizpeGomez <dicom@offis.de>
2018-09-07 11:22:58 +0200

    Updated Checksums.

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -5,4 +5,4 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.3-win32-dynamic.zip' # download url
 $url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.3-win64-dynamic.zip' # download url
 
-Install-ChocolateyZipPackage $packageName $url -Checksum B7AABBEC6DC14F72BAE6A2CC688CB0C0 $toolsDir -Url64 $url64 -Checksum64 87B85476006510DE8308F0DB3475DBA2
+Install-ChocolateyZipPackage $packageName $url -Checksum a0b3d078aafd1b7170bc24eea6c0c9b5 $toolsDir -Url64 $url64 -Checksum64 f8683dfbdeb2df863bd0da8a4d118214
