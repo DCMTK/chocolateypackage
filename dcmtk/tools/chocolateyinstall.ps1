d2dfe871a843eb063cc67874c1f115dded03fe18
Pedro Fernando Arizpe Gomez <pedrofernando.arizpegomez@offis.de>
2020-06-09 15:44:41 +0200

    Updated chocolatey package version to 3.6.5.

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -2,7 +2,9 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.4-win32-dynamic.zip' # download url
-$url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.4-win64-dynamic.zip' # download url
+$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win32-dynamic.zip' # download url
+$url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win64-dynamic.zip' # download url
+$checksum = '94db84f86dbb06448f9c60023fc7ece9'
+$checksum64 = 'a8990313c4df70dc6a8507babf5f5b3ade1bb703'
 
-Install-ChocolateyZipPackage $packageName $url -Checksum 7c398a26909da52a97969a38bc440eb6 $toolsDir -Url64 $url64 -Checksum64 8267c09ee908bbef63eac9f705709a63
+Install-ChocolateyZipPackage $packageName $url -Checksum $checksum $toolsDir -Url64 $url64 -Checksum64 $checksum64
--- dcmtk/tools/chocolateyuninstall.ps1
+++ dcmtk/tools/chocolateyuninstall.ps1
@@ -1,12 +1,12 @@
 $packageName = 'dcmtk'
 
-$zipFileName = 'dcmtk-3.6.4-win32-dynamic.zip'
-$zipFileName64 = 'dcmtk-3.6.4-win64-dynamic.zip'
+$zipFileName = 'dcmtk-3.6.5-win32-dynamic.zip'
+$zipFileName64 = 'dcmtk-3.6.5-win64-dynamic.zip'
 $os = Get-WmiObject -Class Win32_OperatingSystem;
 
 # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
 if ($os.OSarchitecture.Contains("64")) {
   Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName64
 } else {
-  Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName zipFileName
+  Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName $zipFileName
 }
