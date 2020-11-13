4a56a08be9516d7df28ac07d7121c163fd6e5a1e
P Arizpe Gomez <37437621+pfagomez@users.noreply.github.com>
2020-11-13 11:40:22 +0100

    Changed download location to redmine

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win32-dynamic.zip' # download url
-$url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win64-dynamic.zip' # download url
+$url = 'https://support.dcmtk.org/redmine/attachments/download/172/dcmtk-3.6.5-win32-dynamic.zip' # download url
+$url64 = 'https://support.dcmtk.org/redmine/attachments/download/171/dcmtk-3.6.5-win64-dynamic.zip' # download url
 $checksum = '94db84f86dbb06448f9c60023fc7ece9'
 $checksum64 = '63ECDB9694B764396CA0848D85C90F8F'
 
