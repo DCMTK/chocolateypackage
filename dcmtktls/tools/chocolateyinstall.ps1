ea28db15a651665f9a1801e6b9d60f934af863a3
P Arizpe Gomez <37437621+pfagomez@users.noreply.github.com>
2020-11-13 11:38:15 +0100

    Update chocolateyinstall.ps1
    Changed download urls to redmine

--- dcmtktls/tools/chocolateyinstall.ps1
+++ dcmtktls/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'dcmtktls' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win32-dynamic-tls-complete.zip' # download url
-$url64 = 'ftp://dicom.offis.de/pub/dicom/offis/software/dcmtk/release/bin/dcmtk-3.6.5-win64-dynamic-tls-complete.zip' # download url
+$url = 'https://support.dcmtk.org/redmine/attachments/download/169/dcmtk-3.6.5-win32-dynamic-tls-complete.zip' # download url
+$url64 = 'https://support.dcmtk.org/redmine/attachments/download/170/dcmtk-3.6.5-win64-dynamic-tls-complete.zip' # download url
 $checksum = '72a97f9d4063753d2295e133c38d9f40'
 $checksum64 = '798b1ad4105967a793268140d8e5613d'
 
