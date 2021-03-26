f00c7e165f58a634769d025afaceb02545a992af
Pedro Fernando Arizpe Gomez <pedrofernando.arizpegomez@offis.de>
2021-03-26 12:13:27 +0100

    Removed random newlines from dcmtktls.

--- dcmtktls/tools/chocolateyinstall.ps1
+++ dcmtktls/tools/chocolateyinstall.ps1
@@ -2,10 +2,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'dcmtktls' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = '
-https://dcmtk.org/chocolatey/dcmtk-3.6.6-win32-chocolatey-tls.zip' # download url
-$url64 = '
-https://dcmtk.org/chocolatey/dcmtk-3.6.6-win64-chocolatey-tls.zip' # download url
+$url = 'https://dcmtk.org/chocolatey/dcmtk-3.6.6-win32-chocolatey-tls.zip' # download url
+$url64 = 'https://dcmtk.org/chocolatey/dcmtk-3.6.6-win64-chocolatey-tls.zip' # download url
 $checksum = 'd40e417e40b93b93386ecd969aaa9ca7'
 $checksum64 = '54784ec411e648b60d4a5a32dc69d9eb'
 
