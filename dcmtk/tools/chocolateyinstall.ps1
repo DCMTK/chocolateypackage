5f508c55f990435861a32a150fce02e663c8df64
Marco Eichelberg <dicom@offis.de>
2022-05-09 18:23:34 +0200

    Fixed typo in filename.

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://dcmtk.org/chocolatey/dcmtk-3.6.7-win32-chocolatey.zip' # download url
-$url64 = 'https://dcmtk.org/chocolatey/ddcmtk-3.6.7-win64-chocolatey.zip' # download url
+$url64 = 'https://dcmtk.org/chocolatey/dcmtk-3.6.7-win64-chocolatey.zip' # download url
 $checksum = '5543210cab304792da9041081401e9a5'
 $checksum64 = '32b8d23a7e34565c1da9ad74aa153f0e'
 
