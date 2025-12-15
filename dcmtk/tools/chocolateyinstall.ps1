f0cf7f7d5648b37de5f7611e50a261fda2224174
Marco Eichelberg <dicom@offis.de>
2025-12-15 12:41:14 +0100

    Updates for DCMTK 3.7.0.

--- dcmtk/tools/chocolateyinstall.ps1
+++ dcmtk/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'dcmtk' # arbitrary name for the package, used in messages
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dcmtk.org/chocolatey/dcmtk-3.6.9-win32-chocolatey.zip' # download url
-$url64 = 'https://dcmtk.org/chocolatey/dcmtk-3.6.9-win64-chocolatey.zip' # download url
-$checksum = '4fb11b460e779815f38c3d8de588ab0f'
-$checksum64 = 'b8a0c0038c8ba36ae78a0676353c8f80'
+$url = 'https://dcmtk.org/chocolatey/dcmtk-3.7.0-win32-chocolatey.zip' # download url
+$url64 = 'https://dcmtk.org/chocolatey/dcmtk-3.7.0-win64-chocolatey.zip' # download url
+$checksum = 'a981148be73083ff96d939ba9f3488eb'
+$checksum64 = '0deddf2aba241b7869b89a053abce2de'
 
 Install-ChocolateyZipPackage $packageName $url -Checksum $checksum $toolsDir -Url64 $url64 -Checksum64 $checksum64
